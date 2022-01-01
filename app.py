0ff47f8c00f0a865675ecfe9945f5b1243bb9b95
Rahul Mahato <65120414+rm02021999@users.noreply.github.com>
2022-01-01 22:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,11 @@
 from flask import Flask, render_template, request, Markup
 import numpy as np
 import pandas as pd
-from utils.disease import disease_dic
-from utils.fertilizer import fertilizer_dic
 import requests
 import config
 import pickle
 import io
-import torch
-from torchvision import transforms
 from PIL import Image
-from utils.model import ResNet9
 # ==============================================================================================
 
 # -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
