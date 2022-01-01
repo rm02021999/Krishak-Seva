93340774da9a9e610fe7cbaa42a96cbcef44aaa9
Rahul Mahato <65120414+rm02021999@users.noreply.github.com>
2022-01-01 21:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,11 +58,6 @@ disease_classes = ['Apple___Apple_scab',
                    'Tomato___Tomato_mosaic_virus',
                    'Tomato___healthy']
 
-disease_model_path = 'models/plant_disease_model.pth'
-disease_model = ResNet9(3, len(disease_classes))
-disease_model.load_state_dict(torch.load(
-    disease_model_path, map_location=torch.device('cpu')))
-disease_model.eval()
 
 
 # Loading crop recommendation model
@@ -100,35 +95,7 @@ def weather_fetch(city_name):
         return None
 
 
-def predict_image(img, model=disease_model):
-    """
-    Transforms image to tensor and predicts disease label
-    :params: image
-    :return: prediction (string)
-    """
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.ToTensor(),
-    ])
-    image = Image.open(io.BytesIO(img))
-    img_t = transform(image)
-    img_u = torch.unsqueeze(img_t, 0)
-
-    # Get predictions from model
-    yb = model(img_u)
-    # Pick index with highest probability
-    _, preds = torch.max(yb, dim=1)
-    prediction = disease_classes[preds[0].item()]
-    # Retrieve the class label
-    return prediction
-
-# ===============================================================================================
-# ------------------------------------ FLASK APP -------------------------------------------------
-
-
 app = Flask(__name__)
-
-# render home page
 
 
 @ app.route('/')
@@ -238,26 +205,6 @@ def fert_recommend():
 # render disease prediction result page
 
 
-@app.route('/disease-predict', methods=['GET', 'POST'])
-def disease_prediction():
-    title = 'Harvestify - Disease Detection'
-
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files.get('file')
-        if not file:
-            return render_template('disease.html', title=title)
-        try:
-            img = file.read()
-
-            prediction = predict_image(img)
-
-            prediction = Markup(str(disease_dic[prediction]))
-            return render_template('disease-result.html', prediction=prediction, title=title)
-        except:
-            pass
-    return render_template('disease.html', title=title)
 
 
 # ===============================================================================================
